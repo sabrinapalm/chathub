c156e0070794718d7d130a2754d18ad63b203d32
sabrina palm <sabrina_palm@hotmail.com>
2018-02-15 19:19:20 +0100

    +

--- authorization.js
+++ authorization.js
@@ -9,71 +9,26 @@ githubLogin.addEventListener('click', function(event){
 })
     
     
-var provider = new firebase.auth.GithubAuthProvider();
-
-provider.addScope('repo');
+let provider = new firebase.auth.GithubAuthProvider();
 
 
-provider.setCustomParameters({
-  'allow_signup': 'false'
-});
-
-
-firebase.auth().signInWithPopup(provider).then(function(result) {
-  // This gives you a GitHub Access Token. You can use it to access the GitHub API.
-  var token = result.credential.accessToken;
-  // The signed-in user info.
-  var user = result.user;
-  // ...
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ...
-});
-    
-    
-firebase.auth().signInWithRedirect(provider);
-
-
-firebase.auth().getRedirectResult().then(function(result) {
-  if (result.credential) {
-    // This gives you a GitHub Access Token. You can use it to access the GitHub API.
-    var token = result.credential.accessToken;
-    // ...
-  }
-  // The signed-in user info.
-  var user = result.user;
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ...
+firebase.auth().signInWithPopup(provider)
+    .then(function(result) {
+	// Om autentisering lyckas, så finns användarinfo i user
+	let user = result.user;
 });
     
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+// Logga ut den autentiserade användaren
+firebase.auth().signOut()
+.then(function(result) {
+	// Utloggning lyckades
+})
+.catch(function(error) {
+	// Utloggning misslyckades
+});
+
     
     
     
